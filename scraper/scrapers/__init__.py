a27ef0ea8602f4e4528b7fd94b5294e9c8ff8b6e
Ghazala Mirani <ghazalasahilmirani7@gmail.com>
2026-07-26 19:22:03 -0400

    Add curated seed scholarships for all study levels

--- scraper/scrapers/__init__.py
+++ scraper/scrapers/__init__.py
@@ -1,8 +1,10 @@
 from .scholarships_canada import ScholarshipsCanadaScraper
 from .universities import UniversityScraper
 from .government import GovernmentScraper
+from .seed import SeedScraper
 
 ALL_SCRAPERS = [
+    SeedScraper,
     GovernmentScraper,
     UniversityScraper,
     ScholarshipsCanadaScraper,
